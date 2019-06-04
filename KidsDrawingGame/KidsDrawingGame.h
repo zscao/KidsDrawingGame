569ffa4c569e7365da034f1b97452c75895a3f03
zscao <caozsn@gmail.com>
2019-06-04 17:42:56 +1000

    Initial Commit

---
+++ KidsDrawingGame/KidsDrawingGame.h
@@ -0,0 +1,19 @@
+//
+//  KidsDrawingGame.h
+//  KidsDrawingGame
+//
+//  Created by matchbyte on 4/6/19.
+//  Copyright © 2019 zscao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KidsDrawingGame.
+FOUNDATION_EXPORT double KidsDrawingGameVersionNumber;
+
+//! Project version string for KidsDrawingGame.
+FOUNDATION_EXPORT const unsigned char KidsDrawingGameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KidsDrawingGame/PublicHeader.h>
+
+
